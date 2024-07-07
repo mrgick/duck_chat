fc65cf2903e05f427c92f2e7aa7fabbde41af2c0
mrgick <mrgickcool@gmail.com>
2024-07-07 16:06:26 +0300

    Add interactive example

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import asyncio
 
 async def main():
     async with DuckChat(model=ModelType.GPT3) as chat:
-        print(await chat.ask_question("2+2?"))
-        print(await chat.ask_question("Are you right?"))
-        print(await chat.ask_question("Thank you, have a good day)"))
+        while True:
+            question = input()
+            if question == "stop":
+                break
+            print(await chat.ask_question(question))
 
 
 asyncio.run(main())
