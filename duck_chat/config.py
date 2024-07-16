09332f5b4c6889dd3e64477d49a961f09327215f
k-aito <77667659+k-aito@users.noreply.github.com>
2024-07-16 17:13:45 +0200

    Add support for ~/.config/hey/conf.toml configuration file

--- duck_chat/config.py
+++ duck_chat/config.py
@@ -1,3 +1,6 @@
+import os
+import toml
+
 from enum import Enum
 
 
@@ -6,3 +9,16 @@ class ModelType(Enum):
     GPT3 = "gpt-3.5-turbo-0125"
     Llama = "meta-llama/Llama-3-70b-chat-hf"
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+    def read_model_from_conf():
+        filepath = os.environ["HOME"] + '/.config/hey/conf.toml'
+        if os.path.exists(filepath):
+            with open(filepath, 'r') as f:
+                conf = toml.load(f)
+                model_name = conf['model']
+            if ModelType[model_name]:
+                return ModelType[model_name]
+            else:
+                return ModelType.Claude
+        else:
+            return ModelType.Claude
