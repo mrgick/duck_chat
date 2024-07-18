f0537d8a4e18f91adf493ad8b3ed4505ed8b518d
mrgick <mrgickcool@gmail.com>
2024-07-18 22:14:45 +0300

    Some refactoring

--- duck_chat/config.py
+++ duck_chat/config.py
@@ -1,7 +1,7 @@
-import os
-import toml
-
 from enum import Enum
+from pathlib import Path
+
+import toml
 
 
 class ModelType(Enum):
@@ -11,14 +11,11 @@ class ModelType(Enum):
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
     def read_model_from_conf():
-        filepath = os.environ["HOME"] + '/.config/hey/conf.toml'
-        if os.path.exists(filepath):
-            with open(filepath, 'r') as f:
+        filepath = Path.home() / ".config" / "hey" / "conf.toml"
+        if filepath.exists():
+            with open(filepath, "r") as f:
                 conf = toml.load(f)
-                model_name = conf['model']
+                model_name = conf["model"]
             if ModelType[model_name]:
                 return ModelType[model_name]
-            else:
-                return ModelType.Claude
-        else:
-            return ModelType.Claude
+        return ModelType.Claude
