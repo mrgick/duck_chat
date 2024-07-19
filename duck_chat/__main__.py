c2a96e123b57e75c0030c27c30f809fc4b704609
k-aito <77667659+k-aito@users.noreply.github.com>
2024-07-19 20:09:17 +0200

    Add display identifier for user input and response for preparing multiline input and answers

--- duck_chat/__main__.py
+++ duck_chat/__main__.py
@@ -7,13 +7,16 @@ from .config import ModelType
 
 async def main():
     async with DuckChat(model=ModelType.read_model_from_conf()) as chat:
+        print('Type \033[1;4mstop\033[0m to quit')
         while True:
             try:
+                print("\033[1;4m>>> User input:\033[0m", end="\n")
                 question = input()
             except EOFError:
                 sys.exit(0)
             if question == "stop":
                 break
+            print("\033[1;4m>>> Response:\033[0m", end="\n")
             print(await chat.ask_question(question))
 
 
