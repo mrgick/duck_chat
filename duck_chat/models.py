d3bd738fc4e9389367982c59078de8a86491841f
Larry Igna <162085428+Troptrap@users.noreply.github.com>
2024-08-15 11:48:17 +0300

    Update models.py
    Typo

--- duck_chat/models.py
+++ duck_chat/models.py
@@ -4,7 +4,7 @@ import msgspec
 
 
 class ModelType(Enum):
-    Claude = "claude-3-haiku-20240307
+    Claude = "claude-3-haiku-20240307"
     Llama = "meta-llama/Meta-Llama-3-70B-Instruct-Turbo"
     GPT4o = "gpt-4o-mini"
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
