729751d8fd3db4a30fee8dcaa57034fe4c194fc3
Larry Igna <162085428+Troptrap@users.noreply.github.com>
2024-08-15 11:44:42 +0300

    Update models.py
    Changed Llama model to `meta-llama/Meta-Llama-3-70B-Instruct-Turbo`

--- duck_chat/models.py
+++ duck_chat/models.py
@@ -4,9 +4,8 @@ import msgspec
 
 
 class ModelType(Enum):
-    Claude = "claude-3-haiku-20240307"
-    GPT3 = "gpt-3.5-turbo-0125"  # Deprecated
-    Llama = "meta-llama/Llama-3-70b-chat-hf"
+    Claude = "claude-3-haiku-20240307
+    Llama = "meta-llama/Meta-Llama-3-70B-Instruct-Turbo"
     GPT4o = "gpt-4o-mini"
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
