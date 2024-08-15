f7270dfc399cadffd31a71e2bdfef87685c9095f
Troptrap <laurexweb@gmail.com>
2024-08-15 11:52:23 +0300

    modified:   duck_chat/models.py

--- duck_chat/models.py
+++ duck_chat/models.py
@@ -5,7 +5,7 @@ import msgspec
 
 class ModelType(Enum):
     Claude = "claude-3-haiku-20240307"
-    Llama = "meta-llama/Meta-Llama-3-70B-Instruct-Turbo"
+    Llama = "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
     GPT4o = "gpt-4o-mini"
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
